38cec5a668a668b0e53581e44db783ba77951179
GregoryPerifanos <g.perifanos89@gmail.com>
2024-04-07 18:24:45 +0300

    First commit

---
+++ src/main/java/gr/aueb/cf/eshopfinalproject/EshopFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eshopfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/eshopfinalproject/EshopFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eshopfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
